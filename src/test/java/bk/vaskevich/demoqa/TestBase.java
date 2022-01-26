28a46077a07b8507b68805a8c5addd0a16fed66a
SvetlanaVaskevich <81557049+SvetlanaVaskevich@users.noreply.github.com>
2022-01-26 16:07:58 +0500

    Added TestBase and Attach

--- src/test/java/bk/vaskevich/demoqa/TestBase.java
+++ src/test/java/bk/vaskevich/demoqa/TestBase.java
@@ -21,8 +21,8 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC","true");
-        capabilities.setCapability("enableVideo","false");
+        capabilities.setCapability("enableVNC",true);
+        capabilities.setCapability("enableVideo",false);
         Configuration.browserCapabilities = capabilities;
     }
 
